22482d4dc0f3acc7bab075e1eaa6e8a0cacfa3fa
Galiia Uzbekova <galiiauzbekova@gmail.com>
2023-03-02 01:24:13 +0500

    created tests to verify the correct page

---
+++ src/test/java/github/SelenideVerifyPage.java
@@ -0,0 +1,18 @@
+package github;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideVerifyPage {
+
+    @Test
+    void verifyCorrectPage() {
+        open("https://github.com/");
+        $(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $("body").shouldHave(text("Build like the best"));
+    }
+}
